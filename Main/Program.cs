5acaf93b67dc0fb4af9c7f79bd104bab8e85c868
Oleksandr <sorokin.biochemistry@gmail.com>
2020-11-18 16:51:01 +0100

    modified launcher

--- Main/Program.cs
+++ Main/Program.cs
@@ -7,28 +7,45 @@ namespace WeatherSubscribeService
 {
     public class Program
     {
-        static JsonFileContent configContent = new JsonFileContent("configs.json");
-        static Consumer consumer = new Consumer(
+        JsonFileContent configContent;
+        Consumer consumer;
+        Publisher publisher;
+        MySqlDatabaseClient databaseClient;
+        SubscriptionFacade facade;
+
+        public Program()
+        {
+            configContent = new JsonFileContent("configs.json");
+            consumer = new Consumer(
             configContent.Value("RabbitUrl").ToString(),
             configContent.Value("RabbitLogin").ToString(),
             configContent.Value("RabbitPassword").ToString()
             );
-        static Publisher publisher = new Publisher(
-            configContent.Value("RabbitUrl").ToString(),
-            configContent.Value("RabbitLogin").ToString(),
-            configContent.Value("RabbitPassword").ToString()
-            );
-        static MySqlDatabaseClient databaseClient = new MySqlDatabaseClient(
-            configContent.Value("MySqlServer").ToString(),
-            configContent.Value("MySqlDatabase").ToString(),
-            configContent.Value("MySqlLogin").ToString(),
-            configContent.Value("MySqlPassword").ToString()
-            );
-        static SubscriptionFacade facade = new SubscriptionFacade("configs.json", consumer, publisher, databaseClient);
+            publisher = new Publisher(
+                configContent.Value("RabbitUrl").ToString(),
+                configContent.Value("RabbitLogin").ToString(),
+                configContent.Value("RabbitPassword").ToString()
+                );
+            databaseClient = new MySqlDatabaseClient(
+                configContent.Value("MySqlServer").ToString(),
+                configContent.Value("MySqlDatabase").ToString(),
+                configContent.Value("MySqlLogin").ToString(),
+                configContent.Value("MySqlPassword").ToString()
+                );
+            facade = new SubscriptionFacade("configs.json", consumer, publisher, databaseClient);
+        }
+
+        public void Start()
+        {
+            facade.Run();
+        }
+        
+        
 
         public static void Main(string[] args)
         {
-            facade.Run();
+            var launcer = new Program();
+            launcer.Start();
         }
     }
 }
