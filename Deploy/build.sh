83e5a1617b2089b3194557057760fc0eb603d4e0
Oleksandr <sorokin.biochemistry@gmail.com>
2020-12-09 18:25:43 +0100

    deploy update

--- Deploy/build.sh
+++ Deploy/build.sh
@@ -5,5 +5,6 @@ cd ..
 dotnet build WeatherSubscribeService.sln -c Release -o "src/"
 echo "WeatherSubscribeService has been built."
 echo "Building container..."
+cd Deploy
 docker build Dockerfile -t subscribe
 echo "Container has been built."
